94e4120014f6613fd02f925630fd73d71f643a9c
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-07-25 23:48:39 +0800

    Added the implementation of Cors to be global

--- src/main/java/com/shop/ShopApplication/Configuration/SecurityConfig.java
+++ src/main/java/com/shop/ShopApplication/Configuration/SecurityConfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +44,7 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults()) // this lets you test endpoints quickly from tools like postman.
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // this makes every http request stateless, meaning every request should contain a token.
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class) // before using the UsernamePasswordAuthenticationFilter go to JWTFilter first
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // include the cors configuration
                 .build();
     }
 
@@ -56,5 +62,18 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager(); // gets the object of AuthenticationManager.
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200")); // Allow your frontend origin
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // List of allowed methods
+        config.setAllowedHeaders(List.of("*")); // Allowed headers
+        config.setAllowCredentials(true); // Required for Authorization header or cookies
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Apply to all endpoints.
+        return source;
     }
 }
