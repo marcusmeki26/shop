f4f6b15d9c09fa2856176ea518db3e1b2b71559d
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-07-28 00:03:37 +0800

    Created a repository for category to handle database communication

---
+++ src/main/java/com/shop/ShopApplication/Repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.shop.ShopApplication.Repository;
+
+import com.shop.ShopApplication.Entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
