ee4f2f6874fe6600957aa0f6ecfa9f2a8d59bd79
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-07-27 23:59:03 +0800

    Created a repository for Product

---
+++ src/main/java/com/shop/ShopApplication/Repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.shop.ShopApplication.Repository;
+
+import com.shop.ShopApplication.Dto.ProductDto;
+import com.shop.ShopApplication.Entity.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Products, Integer> {
+//    List<Products> findByUser_id(Integer id); // findByUser the 'User' should match the implement object name inside Product entity and the name after _ should match the column in which entity it has relationship.
+}
