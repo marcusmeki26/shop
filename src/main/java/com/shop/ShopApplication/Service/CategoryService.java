2a460cbb2e57d01f3c7bf0e09ee53261a55c4c99
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-07-28 00:02:57 +0800

    Created a service to handle category's business logics

---
+++ src/main/java/com/shop/ShopApplication/Service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.shop.ShopApplication.Service;
+
+import com.shop.ShopApplication.Entity.Category;
+import com.shop.ShopApplication.Repository.CategoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CategoryService {
+
+    private CategoryRepository repository;
+
+    // Used for fetching all categories
+    public List<Category> getCategories() {
+        return repository.findAll();
+    }
+}
