14ef8e9851b8bf9684ffe5e2ef63e6efd9432c6a
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-07-27 23:59:56 +0800

    Used by mapper to handle fetching user id

---
+++ src/main/java/com/shop/ShopApplication/Resolver/UserResolver.java
@@ -0,0 +1,21 @@
+package com.shop.ShopApplication.Resolver;
+
+import com.shop.ShopApplication.Entity.Users;
+import com.shop.ShopApplication.Exception.ResourceNotFoundException;
+import com.shop.ShopApplication.Repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class UserResolver {
+    UserRepository repository;
+
+    public Integer map(Users user){
+        Users fetchedUser = repository.findById(user.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User does not exist", HttpStatus.NOT_FOUND.value()));
+
+        return fetchedUser.getId();
+    }
+}
