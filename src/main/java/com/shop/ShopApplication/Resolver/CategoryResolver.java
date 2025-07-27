41812a88c9d0e9b86b7ee609b346b042a3997731
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-07-28 00:00:11 +0800

    Used by mapper to handle fetching category id

---
+++ src/main/java/com/shop/ShopApplication/Resolver/CategoryResolver.java
@@ -0,0 +1,21 @@
+package com.shop.ShopApplication.Resolver;
+
+import com.shop.ShopApplication.Entity.Category;
+import com.shop.ShopApplication.Exception.ResourceNotFoundException;
+import com.shop.ShopApplication.Repository.CategoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class CategoryResolver {
+    CategoryRepository repository;
+
+    public Integer map(Category category){
+        Category fetchedCategory = repository.findById(category.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Category does not exist", HttpStatus.NOT_FOUND.value()));
+
+        return fetchedCategory.getId();
+    }
+}
