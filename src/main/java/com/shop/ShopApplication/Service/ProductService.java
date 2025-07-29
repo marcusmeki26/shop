4be9506658c9c608917c252bc985f9eb49c5e136
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-07-29 23:21:57 +0800

    Implemented a method for fetching by product name

--- src/main/java/com/shop/ShopApplication/Service/ProductService.java
+++ src/main/java/com/shop/ShopApplication/Service/ProductService.java
@@ -7,6 +7,8 @@ import com.shop.ShopApplication.Repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -15,8 +17,28 @@ public class ProductService {
     private ProductRepository repository;
     private ProductMapper prodMapper;
 
+    // Fetches all the products
     public List<ProductDto> getProducts(){
         List<Products> products =  repository.findAll(); // Fetches all the product
         return prodMapper.toProductDto(products); // Converts the List<Products> into List<ProductDto>
+    }
+
+    // Fetches all the products with matching name
+    public List<ProductDto> getProductsByName(String productName) {
+        List<Products> products;
+
+        String searchPattern = "%" + productName + "%";
+
+        // If the passed input is plural
+        if(productName.endsWith("s")){
+            String singularForm = "%" + productName.substring(0, productName.length()-1) + "%";
+            products = repository.findByProductNameLikeOrProductNameLike(singularForm, searchPattern);
+        }else{
+            // If the passed input is not plural
+            products = repository.findByProductNameLike(searchPattern);
+        }
+
+
+        return prodMapper.toProductDto(products);
     }
 }
