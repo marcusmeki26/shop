1a41ef5e31303d10cd5bfbad5add77699848f4cd
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-07-29 23:21:09 +0800

    Added two derived query methods for fetching by product name

--- src/main/java/com/shop/ShopApplication/Repository/ProductRepository.java
+++ src/main/java/com/shop/ShopApplication/Repository/ProductRepository.java
@@ -1,10 +1,14 @@
-package com.shop.ShopApplication.Repository.JPA;
+package com.shop.ShopApplication.Repository;
 
 import com.shop.ShopApplication.Entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Products, Integer> {
 //    List<Products> findByUser_id(Integer id); // findByUser the 'User' should match the implement object name inside Product entity and the name after _ should match the column in which entity it has relationship.
+    List<Products> findByProductNameLikeOrProductNameLike(String productName1, String productName2);
+    List<Products> findByProductNameLike(String productName);
 }
