60367fe7661f92d2af516cfb7a127a91cfd09f36
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-07-31 20:25:40 +0800

    Added derived methods for joining tables for feching by category id also filtering by product name

--- src/main/java/com/shop/ShopApplication/Repository/ProductRepository.java
+++ src/main/java/com/shop/ShopApplication/Repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.shop.ShopApplication.Repository;
 
+import com.shop.ShopApplication.Dto.ProductDto;
 import com.shop.ShopApplication.Entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +14,12 @@ public interface ProductRepository extends JpaRepository<Products, Integer> {
 //    List<Products> findByUser_id(Integer id); // findByUser the 'User' should match the implement object name inside Product entity and the name after _ should match the column in which entity it has relationship.
     List<Products> findByProductNameLikeOrProductNameLike(String productName1, String productName2);
     List<Products> findByProductNameLike(String productName);
+    List<Products> findByCategoryId_Id(Integer id);
+    @Query("SELECT new com.shop.ShopApplication.Dto.ProductDto(p.productId, p.productName, p.imagePath, p.price, o.id, c.id) " +
+            "FROM Products p " +
+            "LEFT JOIN p.ownerId o " +
+            "JOIN p.categoryId c " +
+            "WHERE LOWER(p.productName) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "AND c.categoryName = :categoryName")
+    List<ProductDto> findByCategoryIdAndProductId(@Param("keyword") String keyword,@Param("categoryName") String categoryName);
 }
