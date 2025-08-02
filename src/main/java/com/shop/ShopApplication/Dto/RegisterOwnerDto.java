198b379a28af9564bcfb2b92bc1274d2b2292f6d
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-08-02 21:46:43 +0800

    Created to handle owner submission

---
+++ src/main/java/com/shop/ShopApplication/Dto/RegisterOwnerDto.java
@@ -0,0 +1,24 @@
+package com.shop.ShopApplication.Dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RegisterOwnerDto extends RegisterUserDto{
+
+    @NotEmpty(message = "Shop name should not be empty")
+    private String shopName;
+    @NotEmpty(message = "Shop's description should not be empty")
+    private String description;
+    @NotEmpty(message = "Phone number should not be empty")
+    private String phoneNumber;
+    @NotEmpty(message = "Shop image should not be empty")
+    private String shopImage;
+
+}
