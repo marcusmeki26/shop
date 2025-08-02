6240ff5867a0428d3e3e25fde19366172b4f1b9d
Marcus Felixmena <danielmarcusq.felixmena@gmail.com>
2025-08-02 21:35:58 +0800

    Added annotation for Jpa and ElasticRepository and implemented repair for flyway

--- src/main/java/com/shop/ShopApplication/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication/ShopApplication.java
@@ -1,12 +1,23 @@
 package com.shop.ShopApplication;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.shop.ShopApplication.Repository.JpaRepository")
+@EnableElasticsearchRepositories(basePackages = "com.shop.ShopApplication.Repository.ElasticRepository")
 public class ShopApplication {
 
 	public static void main(String[] args) {
+		Flyway flyway = Flyway.configure()
+				.dataSource("jdbc:mariadb://localhost:3306/shop", "root", "")
+				.load();
+		flyway.repair();
+
 		SpringApplication.run(ShopApplication.class, args);
 	}
 }
